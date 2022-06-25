bb1d6b7b2fda0ccf6841afed177a6c1bb3ba8aaf
Martin <mart256@gmail.com>
2022-06-25 17:36:47 +0200

    Added login with state

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,38 @@
+import { useState } from "react";
 
 export default function Login() {
-    return <h1>A login</h1>;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const usernameHandler =  (e) => {
+        setUsername(e.target.value);
+    }
+
+    const passwordHandler =  (e) => {
+        setPassword(e.target.value);
+    }
+
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <div className="imgcontainer">
+                <img src="img_avatar2.png" alt="Avatar" className="avatar"/>
+            </div>
+
+            <div className="container">
+                <label htmlFor="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required
+                    onChange={usernameHandler}/>
+
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required
+                    onChange={passwordHandler}/>
+
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>;
 }
