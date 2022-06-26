55d85e6b7569cd530ec9912218d30a62667244d1
Martin <mart256@gmail.com>
2022-06-26 12:19:45 +0200

    Redirecting to the login if there is no session.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import Head from 'next/head'
+import withSession from "../lib/session";
 
-export default function Home() {
+export default function Home({ user }) {
   return (
     <div>
       <Head>
         <title>Home</title>
       </Head>
 
-      <h2>Home page</h2>
+      <h2>Welcome to the home page {user.username}!</h2>
     </div>
   )
 }
+
+export const getServerSideProps = withSession(async function ({ req, res }) {
+  const user = req.session.get("user");
+
+  if (user === undefined) {
+    res.setHeader("location", "/login");
+    res.statusCode = 302;
+    res.end();
+    return { props: {} };
+  }
+
+  return {
+    props: { user: req.session.get("user") },
+  };
+});
